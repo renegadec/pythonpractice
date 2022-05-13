7f889d13040d190ea9e3b60efc5db726bd50c7b8
africoiner <confidencenyirenda7@gmail.com>
2022-05-13 20:42:39 +0200

    added function to calculate exchange rate for 2 currencies

--- projects/currency-converter.py
+++ projects/currency-converter.py
@@ -6,5 +6,38 @@ API_KEY = "90273ff32d73f1feacad"
 
 printer = PrettyPrinter()
 
-def get_currencies()
-endpoint = f"/api/v7/currencies?apiKey={API_KEY}"
+def get_currencies():
+    endpoint = f"/api/v7/currencies?apiKey={API_KEY}"
+    url = BASE_URL + endpoint
+    data = get(url).json()['results']
+
+    data = list(data.items())
+    data.sort()
+
+    return data
+
+def print_currencies(currencies):
+    for name, currency in currencies:
+        name = currency['currencyName']
+        _id = currency['id']
+        symbol = currency.get("currencySymbol", "")
+        print(f"{_id} - {name} - {symbol}")
+
+def exchange_rate(currency1, currency2):
+    endpoint = f"/api/v7/convert?q={currency1}_{currency2}&compact=ultra&apiKey={API_KEY}"
+    url = BASE_URL + endpoint
+    data = get(url).json()
+
+    if len(data) == 0:
+        print('Invalid currencies')
+        return
+
+    rate = list(data.values())[0]
+    print(f" {currency1} 1 = {currency2} {rate}")
+    return rate
+
+# data = get_currencies()
+# print_currencies(data)
+rate = exchange_rate("USD", "ZWL")
+
+
